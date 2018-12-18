5bb18a0dc32c53484d3982cdf7e2799ea02c6675
Todd Kenzie <mopeybear@gmail.com>
2018-12-18 11:10:35 -0500

    Example of Components using components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
- 
+import Headline from './Headline';
+
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Welcome to React</h1>
+        <Headline />
         <p>This is some basic content.</p>
       </div>
     );
---
+++ src/Headline.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+class Headline extends Component {
+    render() {
+        return (
+            <h1>Welcome to React</h1>
+        );
+    }
+}
+
+export default Headline;
