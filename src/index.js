1938a67985fc504a8adcddbb92f5baee405a2a79
Todd Kenzie <mopeybear@gmail.com>
2018-12-18 10:21:33 -0500

    React initial working

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-document.querySelector('#root').innerHTML = `<h1>Loading...</h1>`
+ReactDOM.render(<h1>Hello, React!</h1>, document.querySelector('#root'));
+
+// import './index.css'
+// document.querySelector('#root').innerHTML = `<h1>Loading...</h1>`
